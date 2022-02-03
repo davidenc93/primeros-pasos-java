2ab46c5aed9a4e2b63bf9f4be9459cc9fe72e2bb
Abraham David <abrahamdavid.ramirez93@gmail.com>
2022-02-03 15:06:14 -0600

    3 de febrero

---
+++ JavaBasicos/src/metodos/Metodos.java
@@ -0,0 +1,37 @@
+package metodos;
+
+public class Metodos {
+		// un metodo es una funcion
+	
+		//atributis de la clase
+		int a, b;
+		
+		//metodo void (vacío)
+		
+		public void sumar() {
+			int resultado = a + b;
+			System.out.println("El resultado de sumar " + a + " mas " + b + " es " + resultado);
+		}
+		
+		//metodo con retorno de valores
+		public int sumaConRetorno() {
+//			int resultado = a + b;
+//			return resultado;
+			return a + b;
+		}
+		
+		//argumentos
+		// suma (argumentos)
+		
+		public double multiplicarConArgumentos(int arg1, double arg2) {
+			int c = arg1;
+			double d = arg2;
+			return c * d;
+		}
+		
+		// Método estático 
+		public static void imprimirNombre(String nombre) {
+			System.out.println("Mi nombre es: " + nombre);
+			
+		}
+}
